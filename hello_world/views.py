c2854ccf9c4c5153e1c101afad44462741836f3d
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-04-11 18:23:44 +0200

    Blad

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Franciszek"
+moje_imie = "anciszek"
 msg = "Hello World!"
 
 @app.route('/')
